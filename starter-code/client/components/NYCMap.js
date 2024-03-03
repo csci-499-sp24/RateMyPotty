074cb2a00cb1a56f884d109d19c5f2f5d95bfcef
Alex Huynh <103627414+alexlehuynh@users.noreply.github.com>
2024-03-02 23:29:02 -0500

    Updated NYCMap
    Wasn't loading on render but was working locally.  Going to try putting the API Key directly into code instead of leaving it in .env

--- starter-code/client/components/NYCMap.js
+++ starter-code/client/components/NYCMap.js
@@ -7,12 +7,26 @@ import {
     InfoWindow,
 } from "@vis.gl/react-google-maps";
 
-
+/*
 export default function NYCMap() {
     const position = { lat: 40.7128, lng: -74.0060 };
 
     return(
         <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+            <div style={{ height: "100vh"}}>
+                <Map zoom={13} center={position}></Map>
+            </div>
+        </APIProvider>
+    )
+}
+
+*/
+
+export default function NYCMap() {
+    const position = { lat: 40.7128, lng: -74.0060 };
+
+    return(
+        <APIProvider apiKey="AIzaSyBMpZf4jDALGdvq2QYbNUl3ZuquMaK7I_k">
             <div style={{ height: "100vh"}}>
                 <Map zoom={13} center={position}></Map>
             </div>
