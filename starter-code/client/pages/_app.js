71b90d083f868bc574ca94a738144d198c17b925
aLostPocky <c.philipcao@gmail.com>
2024-03-07 22:08:53 -0500

    got user's location working

--- starter-code/client/pages/_app.js
+++ starter-code/client/pages/_app.js
@@ -1,7 +1,15 @@
 import {useState} from 'react';
+import React from 'react';
 import NYCMap from '../components/NYCMap.js'; 
 
 function App() {
+  //Asking for user's location
+  React.useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position)
+    })
+  }, [])
+
   return (
     <div className="App">
       <NYCMap />
