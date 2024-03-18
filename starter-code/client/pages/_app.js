06f0015c80b0cd1984ce568495786fa1f2234882
Dimitry Principale <dimitryprincipale@gmail.com>
2024-03-17 23:34:29 -0400

    checking to see if bootstraps still gives issues again

--- starter-code/client/pages/_app.js
+++ starter-code/client/pages/_app.js
@@ -3,7 +3,7 @@ import HomePage from '@/components/HomePage';
 import './App.css'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
