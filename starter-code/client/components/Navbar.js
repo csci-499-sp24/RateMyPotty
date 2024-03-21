5538fcc52630a961ef0b0e880407334e49ec4f55
alexhuynh <alexlehuynh@gmail.com>
2024-03-21 16:43:19 -0400

    minor fix

--- starter-code/client/components/Navbar.js
+++ starter-code/client/components/Navbar.js
@@ -90,7 +90,7 @@ const Navbar = () => {
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
           <Link href="/signup" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
-              Log in / Sign up
+              Get Started
           </Link>
           
 
