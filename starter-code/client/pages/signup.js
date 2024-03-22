b3bf7fa289e5925d8ef5d1bedda0caf84acc2de4
Connie Qiu <connieqiu24@gmail.com>
2024-03-22 15:53:23 -0400

    removed commented out signup code and removed css file for old signup page

--- starter-code/client/pages/signup.js
+++ starter-code/client/pages/signup.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from '../styles/Signup.module.css'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import {useState} from 'react';
@@ -21,10 +20,9 @@ const Signup = () => (
         </div>
         
         <Auth
-            className={styles.authUI}
             supabaseClient={supabase}
             appearance={{ theme: ThemeSupa }}
-            providers={['google', 'facebook', 'twitter']}
+            providers={['google', 'facebook', 'twitter', 'github']}
         />
         
         <div>
@@ -32,54 +30,5 @@ const Signup = () => (
         </div>
     </div>
 )
-
-/*function Signup(){
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    async function signUpClicked(){
-        try{
-            const { data, error } = await supabase.auth.signUp({
-                email,
-                password
-            });
-            if(!error){
-                return <Link href='/'></Link>;
-            }
-        }catch(error){
-            console.log(error.message);
-        }
-    }
-
-    return(
-        <div>
-            <div>
-            <Navbar></Navbar>
-            </div>
-            <div className={styles.formDiv}>
-                <form onSubmit={signUpClicked} className={styles.form}>
-                    <div>
-                        <h1 className={styles.heading}>Welcome to RateMyPotty</h1>
-                        <div>
-                            <label className={styles.label} htmlFor="email">Email</label>
-                            <input className={styles.input} type="email" id="email" name="email" onChange={(e) => setEmail(e.target.value)}></input>
-                        </div>
-                        <div>
-                            <label className={styles.label} htmlFor="password">Password</label>
-                            <input className={styles.input} type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}></input>
-                        </div>
-                        <div>
-                            <button className={styles.signupButton} type="submit">Sign Up</button>
-                        </div>
-                    </div>
-                </form>
-                <div>
-                    <Footer></Footer>
-                </div>
-            </div>
-            
-        </div>
-    );
-}*/
 
 export default Signup;
