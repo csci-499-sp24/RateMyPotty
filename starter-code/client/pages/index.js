a54ef95bd9ae3f3ceee21114329d08a231c3ddb9
Dimitry Principale <dimitryprincipale@gmail.com>
2024-03-23 19:22:08 -0400

    fixed marker text

--- starter-code/client/pages/index.js
+++ starter-code/client/pages/index.js
@@ -12,8 +12,15 @@ import Faq from '../components/faq';
 
 
 function Index({ darkMode, toggleDarkMode }) {
+<<<<<<< HEAD
   const mapRef = useRef(null); //for scrolling reference in hero and NYCMap
   //const defaultPosition = { lat: null, lng: null };
+=======
+
+  const mapRef = useRef(null); //for scrolling reference in hero and NYCMap
+
+  const defaultPosition = { lat: 40.712775, lng: -74.005973 };
+>>>>>>> 9f5cc43 (fixed marker text)
   const geometryLibrary = useMapsLibrary('geometry');
   const map = useMap();
   const [userPosition, setUserPosition] = useState({});
