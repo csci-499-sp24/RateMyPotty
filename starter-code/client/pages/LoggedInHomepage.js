6d54c2e565e56c11717043aea69587439620516e
Dimitry Principale <dimitryprincipale@gmail.com>
2024-03-29 16:48:04 -0400

    loggedInPageProgress

--- starter-code/client/pages/LoggedInHomepage.js
+++ starter-code/client/pages/LoggedInHomepage.js
@@ -1,70 +1,39 @@
-import React, { useState, useEffect, useRef } from 'react'; //added useRef for scroll
+import React, {useState, useEffect, useRef} from 'react';
 import {
   useMapsLibrary,
-  useMap,
+  useMap
 } from "@vis.gl/react-google-maps";
-
-
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
 import NYCMap from '../components/NYCMap';
-import Navbar from '../components/Navbar';
+import LoggedInNavbar from '@/components/LoggedInNavbar';
 import Testimonials from '../components/testimonials';
 import Faq from '../components/faq';
 
-
-function Index({ darkMode, toggleDarkMode }) {
+function LoggedInHomepage({ darkMode, toggleDarkMode }) {
   const mapRef = useRef(null); //for scrolling reference in hero and NYCMap
-  //const defaultPosition = { lat: null, lng: null };
+
+  const defaultPosition = { lat: 40.712775, lng: -74.005973 };
   const geometryLibrary = useMapsLibrary('geometry');
-  const placesLibrary = useMapsLibrary('places');
-
-
   const map = useMap();
   const [userPosition, setUserPosition] = useState({});
   const [bathrooms, setBathrooms] = useState([]);
   const [popupWindow, setPopupWindow] = useState(null);
-  const inputRef = useRef(null);
-  const autoCompleteRef = useRef();
+
 
   // Get user's position
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
+    navigator.geolocation.getCurrentPosition((position) => {
 
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
-        // Store' user's position in state
-        // Updating state tells react to update the map and set the center to the user's location
-        setUserPosition({ lat: lat, lng: lng })
-        //Hunter coords as location for testing
-        //setUserPosition({ lat: 40.7678, lng: -73.9645 })
-      }, (err) => {
-        console.log('user did not allow us to access location')
-      })
-    }
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      // Store' user's position in state
+      // Updating state tells react to update the map and set the center to the user's location
+      setUserPosition({ lat: lat, lng: lng })
+    }, (err) => {
+      console.log('user did not allow us to access location')
+    })
   }, [])
-
-  useEffect(() => {
-    if (placesLibrary) {
-      console.log('places', placesLibrary)
-      autoCompleteRef.current = new placesLibrary.Autocomplete(inputRef.current)
-      autoCompleteRef.current.addListener("place_changed", async function () {
-        const place = await autoCompleteRef.current.getPlace();
-        const lat = place.geometry.location.lat()
-        const lng = place.geometry.location.lng()
-        // Updating state tells react to update the map and set the center to the user's location
-        setPopupWindow(null)
-        setUserPosition({ lat: lat, lng: lng })
-
-        // Scrolls to the map but with a 50px gap at the top to give it some space 
-        const y = mapRef.current.getBoundingClientRect().top
-        window.scrollTo({ top: y - 50 + window.document.documentElement.scrollTop, behavior: 'smooth' })
-      });
-    }
-
-
-  }, [placesLibrary])
 
   // Once we have the user's location, set the map's center to it
   //added a null check 
@@ -110,15 +79,22 @@ function Index({ darkMode, toggleDarkMode }) {
 
     // Open the popup for this bathroom
     setPopupWindow(nearestBathroom)
+
+    // State to handle the visibility of the filter options
+  const [showFilterOptions, setShowFilterOptions] = useState(false);
+
+  // Function to handle filter button click
+  const handleFilterButtonClick = () => {
+    setShowFilterOptions(!showFilterOptions);
   };
 
 
+
+  };
+
   return (
     <div className="container-fluid">
-      <Navbar />
-      <label></label>
-      {/* <input ref={inputRef} /> */}
-
+      <LoggedInNavbar />
 
       <div className="row">
         <div className="col-md-3">
@@ -126,21 +102,16 @@ function Index({ darkMode, toggleDarkMode }) {
         </div>
         <div className="col-md-9">
           <div className="main-content">
-            <Hero handleEmergencyButtonClick={handleEmergencyButtonClick} mapRef={mapRef}
-              inputRef={inputRef}
-            />
+            <Hero handleEmergencyButtonClick={handleEmergencyButtonClick} mapRef={mapRef} /> 
             <div id="map" className="map-container" ref={mapRef}>
-              <NYCMap className="my-map" userPosition={userPosition} bathrooms={bathrooms} popupWindow={popupWindow} setPopupWindow={setPopupWindow} />
-            </div>
+              <NYCMap className="my-map" userPosition={userPosition} bathrooms={bathrooms} popupWindow={popupWindow} setPopupWindow={setPopupWindow}/>
+            </div> 
           </div>
         </div>
       </div>
-      <Faq />
-      <Testimonials />
       <Footer />
     </div>
-
   );
 }
 
-export default Index;
+export default LoggedInHomepage;
