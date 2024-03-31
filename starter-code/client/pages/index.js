a2f0c5e1c8e0a53da33e929c5a96f1d2cacb7716
gius <pullaragius@icloud.com>
2024-03-30 21:40:27 -0400

    Added functionality that allows the user to input a specific location, adjusted scroll when user clicks on emergency button and updated marker to move to the location that user manually inputed

--- starter-code/client/pages/index.js
+++ starter-code/client/pages/index.js
@@ -1,8 +1,10 @@
-import React, {useState, useEffect, useRef} from 'react'; //added useRef for scroll
+import React, { useState, useEffect, useRef } from 'react'; //added useRef for scroll
 import {
   useMapsLibrary,
-  useMap
+  useMap,
 } from "@vis.gl/react-google-maps";
+
+
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
 import NYCMap from '../components/NYCMap';
@@ -15,34 +17,62 @@ function Index({ darkMode, toggleDarkMode }) {
   const mapRef = useRef(null); //for scrolling reference in hero and NYCMap
   //const defaultPosition = { lat: null, lng: null };
   const geometryLibrary = useMapsLibrary('geometry');
+  const placesLibrary = useMapsLibrary('places');
+
+
   const map = useMap();
   const [userPosition, setUserPosition] = useState({});
   const [bathrooms, setBathrooms] = useState([]);
   const [popupWindow, setPopupWindow] = useState(null);
+  const inputRef = useRef(null);
+  const autoCompleteRef = useRef();
 
   // Get user's position
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
 
-      const lat = position.coords.latitude;
-      const lng = position.coords.longitude;
-      // Store' user's position in state
-      // Updating state tells react to update the map and set the center to the user's location
-      setUserPosition({ lat: lat, lng: lng })
-      //Hunter coords as location for testing
-      //setUserPosition({ lat: 40.7678, lng: -73.9645 })
-    }, (err) => {
-      console.log('user did not allow us to access location')
-    })
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        // Store' user's position in state
+        // Updating state tells react to update the map and set the center to the user's location
+        setUserPosition({ lat: lat, lng: lng })
+        //Hunter coords as location for testing
+        //setUserPosition({ lat: 40.7678, lng: -73.9645 })
+      }, (err) => {
+        console.log('user did not allow us to access location')
+      })
+    }
   }, [])
+
+  useEffect(() => {
+    if (placesLibrary) {
+      console.log('places', placesLibrary)
+      autoCompleteRef.current = new placesLibrary.Autocomplete(inputRef.current)
+      autoCompleteRef.current.addListener("place_changed", async function () {
+        const place = await autoCompleteRef.current.getPlace();
+        const lat = place.geometry.location.lat()
+        const lng = place.geometry.location.lng()
+        // Updating state tells react to update the map and set the center to the user's location
+        setPopupWindow(null)
+        setUserPosition({ lat: lat, lng: lng })
+
+        // Scrolls to the map but with a 50px gap at the top to give it some space 
+        const y = mapRef.current.getBoundingClientRect().top
+        window.scrollTo({ top: y - 50 + window.document.documentElement.scrollTop, behavior: 'smooth' })
+      });
+    }
+
+
+  }, [placesLibrary])
 
   // Once we have the user's location, set the map's center to it
   //added a null check 
-useEffect(() => {
-  if (userPosition.lng && userPosition.lat && map) {
-    map.setCenter(userPosition)
-  }
-}, [userPosition, map])
+  useEffect(() => {
+    if (userPosition.lng && userPosition.lat && map) {
+      map.setCenter(userPosition)
+    }
+  }, [userPosition, map])
 
   // Moved the bathrooms state and api call to homepage because we need that data here to find the nearest bathroom
   useEffect(() => {
@@ -82,9 +112,13 @@ useEffect(() => {
     setPopupWindow(nearestBathroom)
   };
 
+
   return (
     <div className="container-fluid">
       <Navbar />
+      <label></label>
+      {/* <input ref={inputRef} /> */}
+
 
       <div className="row">
         <div className="col-md-3">
@@ -92,10 +126,12 @@ useEffect(() => {
         </div>
         <div className="col-md-9">
           <div className="main-content">
-          <Hero handleEmergencyButtonClick={handleEmergencyButtonClick} mapRef={mapRef} /> 
+            <Hero handleEmergencyButtonClick={handleEmergencyButtonClick} mapRef={mapRef}
+              inputRef={inputRef}
+            />
             <div id="map" className="map-container" ref={mapRef}>
-            <NYCMap className="my-map" userPosition={userPosition} bathrooms={bathrooms} popupWindow={popupWindow} setPopupWindow={setPopupWindow}/>
-            </div> 
+              <NYCMap className="my-map" userPosition={userPosition} bathrooms={bathrooms} popupWindow={popupWindow} setPopupWindow={setPopupWindow} />
+            </div>
           </div>
         </div>
       </div>
