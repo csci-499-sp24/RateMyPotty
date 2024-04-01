b394e52171f91cd71c9cfbe47978486226472c96
Dimitry Principale <dimitryprincipale@gmail.com>
2024-04-01 00:46:51 -0400

    Added logged in page with new changes

--- starter-code/client/components/LoggedInHero.js
+++ starter-code/client/components/LoggedInHero.js
@@ -6,7 +6,9 @@ import emergencyLogo from "../public/emergencylogo.png";
 const LoggedInHero = (props) => {
   const handleEmergencyButtonClick = () => {
     props.handleEmergencyButtonClick();
-    props.mapRef.current.scrollIntoView({ behavior: 'smooth' });
+    // Scrolls to the map but with a 50px gap at the top to give it some space 
+    const y = props.mapRef.current.getBoundingClientRect().top
+    window.scrollTo({ top: y - 50 + window.document.documentElement.scrollTop, behavior: 'smooth' })
   }
   return (
     <>
@@ -14,34 +16,26 @@ const LoggedInHero = (props) => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Welcome back ____
+              Welcome back, NYC
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              Click the Emegergency button or enter a location.
+              Continue making your commute and restroom trips as seamless as possible. Need a restroom urgently? Click the button for the nearest one.
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <button
                 onClick={handleEmergencyButtonClick}
-                className="flex items-center px-8 py-4 h-16 text-2xl font-medium text-center text-white bg-red-600 rounded-md ">
-                <Image 
+                className="flex items-center px-12 py-4 h-12 text-xl font-medium text-center text-white bg-red-600 rounded-md ">
+                <Image
                   src={emergencyLogo}
                   width={24}
-                  height={16}
-                  className="mr-2"
-                  
-                  />
+                  alt="Picture of man needing to use the bathroom"
+                  className="mr-2 h-8"
+                />
                 Emergency
-
               </button>
-              <a
-                href="https://github.com/csci-499-sp24/RateMyPotty"
-                target="_blank"
-                rel="noopener"
-                className="flex items-center space-x-2 text-gray-500 dark:text-gray-400">
-                <GitHubLogo />
-                <span> View on Github</span>
-              </a>
+              <p className="text-xl ml-24 my-4">Or</p>
+              <input className="py-3 pl-6 pr-2 rounded-md text-lg" ref={props.inputRef} />
             </div>
           </div>
         </div>
@@ -61,10 +55,15 @@ const LoggedInHero = (props) => {
       </Container>
       <Container>
         <div className="flex flex-col justify-center">
+          <div className="text-xl text-center text-gray-700 dark:text-white">
+            Trusted by many <span className="text-indigo-600">New Yorkers</span>{" "}
+            citywide
+          </div>
 
           <div className="flex flex-wrap justify-center gap-5 mt-10 md:justify-around">
 
             <div className="pt-2 text-gray-400 dark:text-gray-400">
+              <GitHubLogo />
             </div>
           </div>
         </div>
