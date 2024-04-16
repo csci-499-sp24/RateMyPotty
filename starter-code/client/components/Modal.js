42746862cfd4c12384a59b157ce5d31acdec1f1b
pasteldetective <pi.jackyy@gmail.com>
2024-04-16 09:34:51 -0400

    added documentation

--- starter-code/client/components/Modal.js
+++ starter-code/client/components/Modal.js
@@ -21,7 +21,7 @@ const Modal = ({ isOpen, onClose, selectedName, selectedAddress, selectedReview,
 
     if (!isOpen) return null;
 
-    /*Placeholder imageUrl until able to pass images into Modal.js component*/
+    /*Placeholder imageUrl until able to pass images into Modal.js component, url can be fetched with images from database*/
     const imageUrl = "https://www.heringinternational.com/fileadmin/images/content/de/sanitaer/cwc/Goch-Bahnhof-CWC_R_110_B_960_.jpg"; 
     
 
