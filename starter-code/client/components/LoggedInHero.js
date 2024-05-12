8f426dc1ce8400f9dbfffbcf352922ffecb92fb4
aLostPocky <c.philipcao@gmail.com>
2024-05-11 23:53:21 -0400

    differientiated mobile and desktop views for LoggedInHero.

--- starter-code/client/components/LoggedInHero.js
+++ starter-code/client/components/LoggedInHero.js
@@ -14,6 +14,7 @@ const LoggedInHero = (props) => {
     <>
       <Container>
         
+          <div className="block sm:hidden">
             <div className="flex justify-center items-center flex-wrap">
               <button
                 onClick={handleEmergencyButtonClick}
@@ -29,7 +30,29 @@ const LoggedInHero = (props) => {
               <p className="text-sm ml-3 "></p>
               <input className="py-3 pl-6 pr-2 rounded-md text-sm" ref={props.inputRef} placeholder="Enter a location" />
             </div>
+          </div>
          
+         <div className="hidden sm:block">
+         <div className="flex flex-col items-center justify-center space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row mx-auto">
+          <div className="max-w-2xl mb-8">
+            <div className="flex flex-col items-center justify-center space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+              <button
+                onClick={handleEmergencyButtonClick}
+                className="flex items-center px-12 py-4 h-12 text-xl font-medium text-center text-white bg-red-600 rounded-md ">
+                <Image
+                  src={emergencyLogo}
+                  width={24}
+                  alt="Picture of man needing to use the bathroom"
+                  className="mr-2 h-8"
+                />
+                Emergency
+              </button>
+              <p className="text-xl ml-24 my-4">Or</p>
+              <input className="py-3 pl-6 pr-2 rounded-md text-lg" ref={props.inputRef} placeholder="Enter a location" />
+            </div>
+          </div>
+        </div>
+         </div>
       </Container>
     </>
   );
